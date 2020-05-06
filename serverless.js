02d53e31dc479978a40bb2ee4f194f10d5aaca55
Fedor Parfenov <fvpDev@outlook.com>
2020-05-06 17:11:39 -0400

    init

--- serverless.js
+++ serverless.js
@@ -5,6 +5,6 @@ const build = require('./serverless-build')
 const app = build()
 
 module.exports = async function (req, res) {
-  await app.ready()
+  await app.ready() 
   app.server.emit('request', req, res)
 }
